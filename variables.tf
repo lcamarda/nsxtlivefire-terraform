fa7358fa294e6a1d8e8e5991f9118951c96adf51
lcamarda <camarda.luca@gmail.com>
2019-01-18 09:24:17 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "nsx_ip" {
+  default = "nsxmgr-01a.corp.local"
+}
+
+variable "nsx_password" {
+  default = "VMware1!"
+}
+
+variable "vsphere_user" {
+  default = "administrator@vsphere.local"
+}
+
+variable "vsphere_password" {
+  default = "VMware1!"
+}
+
+variable "vsphere_server" {
+  default = "vcsa-01a.corp.local"
+}
+
+variable "tenant_name" {
+}
+
+variable "web_ip" {
+}
+
+variable "app_ip" {
+}
+
+variable "db_ip" {
+}
